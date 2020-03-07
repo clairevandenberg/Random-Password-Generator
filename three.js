3b59c87608b3ceb03b7d1690a2e97b79b8581784
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-07 15:04:32 +1030

    created loops to generate random letters, symbols & numbers

--- three.js
+++ three.js
@@ -1,5 +1,14 @@
+// Promt to open page to password generator.
+var length = prompt ("Do You Want To Generate a Secure Password")
+// make this promt unbreakable add yes and no.
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characterAmountNumber = documnet.querySelector("#amountNumber");
+var includeLowercase = document.querySelector("#lowercase");
+var includeUppercase = document.querySelector("#uppercase");
+var includeNumbers = document.querySelector("#number");
+var includeSymbols = document.querySelector("#symbol");
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +16,43 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
+   // Loop lowercase and Generate a random lowercase letter
+  function lowercase () {
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var randomlowercase = Math.floor(Math.random() *) + 1;
+
+  console.log(num);
+  return randomlowercase;
 }
+
+   // Loop Symbols and Generate a random symbol 
+   function symbol () {
+   var symbol = '!@#$%^&*=-_';
+    var randomsymbol = Math.floor(Math.random() *) + 1;
+    
+    console.log(symbol);
+    return randomsymbol;
+  }
+
+  // Loop Uppercase and Generate a random uppercase letter 
+  function uppercase() {
+  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var randomuppercase = Math.floor(Math.random() *) + 1;
+    
+    console.log(uppercase);
+    return randomuppercase;
+  }
+
+  // Loop numbers and Generate a random number
+  function numbers () {
+  var number = '0123456789';
+  var randomlowercase = Math.floor(Math.random() *) + 1;
+    
+    console.log(number);
+    return randomlowercase
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
